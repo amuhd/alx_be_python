981965a6a87d7f23f1f0ddcf6d28aa618bcc6315
Muhammed Abdulmalik <abd.muhd01@gmail.com>
2024-06-19 00:26:10 +0100

    Calculation of future age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+# prompt
+current_age = int(input("How old are you? "))
+# future age calculation
+current_year = 2023
+future_year = 2050
+year_addition = future_year - current_year
+future_age = current_age + year_addition
+print(f'In 2050, you will be {future_age} years old.')
