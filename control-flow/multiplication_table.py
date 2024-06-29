bc4e4b61de8f534928401e3f826139ae5a08906c
Muhammed Abdulmalik <abd.muhd01@gmail.com>
2024-06-29 20:45:31 +0100

    Multiplication of numbers

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,13 @@
+# multiplication_table.py
+
+def main():
+    # Prompt users to enter number
+    number = int(input("Enter a number to see its multiplication table: "))
+    
+    # Generation and output of the multiplication table
+    for i in range(1, 10+1):
+        product = number * i
+        print(f"{number} * {i} = {product}")
+
+if __name__ == "__main__":
+    main()
