aa9eeabe309c02e49533d92a755779c877c6d390
Muhammed Abdulmalik <abd.muhd01@gmail.com>
2024-07-12 22:01:23 +0300

    Division calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,11 +5,11 @@ def safe_divide(numerator, denominator):
         if denom == 0:
             return "Error: Cannot divide by zero."
         result = num / denom
-        return f"The result of the dividion is {result}"
+        return f"The result of the division is {result}"
     except ValueError:
         return "Error: Please enter numeric values only."
 
 # Example:
-# print(safe_divide(10, 2))  "The result of the division is: 5.0"
+# print(safe_divide(10, 2))  "The result of the division is 5.0"
 # print(safe_divide(10, 0))  "Cannot divide by zero."
 # print(safe_divide(10, "a"))  "Please enter numeric values only."
