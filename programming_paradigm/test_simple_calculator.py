948554f4586a9882c45bffe62560325cc4d1f031
Muhammed Abdulmalik <abd.muhd01@gmail.com>
2024-07-12 22:16:44 +0300

    Simple calculator and tester

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,7 +6,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def test_addition(self):
-        self.assertEqual(self.calc.addition(1, 2), 3)
+        self.assertEqual(self.calc.addition(2, 3), 5)
         self.assertEqual(self.calc.addition(-1, 1), 0)
         self.assertEqual(self.calc.addition(0, 0), 0)
 
@@ -15,16 +15,16 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtraction(-1, 1), -2)
         self.assertEqual(self.calc.subtraction(0, 0), 0)
 
-    def test_multiply(self):
-        self.assertEqual(self.calc.multiply(3, 5), 15)
-        self.assertEqual(self.calc.multiply(-1, 1), -1)
-        self.assertEqual(self.calc.multiply(0, 100), 0)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiplication(3, 5), 15)
+        self.assertEqual(self.calc.multiplication(-1, 1), -1)
+        self.assertEqual(self.calc.multiplication(0, 100), 0)
 
-    def test_divide(self):
-        self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(-10, 2), -5)
-        self.assertEqual(self.calc.divide(0, 1), 0)
-        self.assertEqual(self.calc.divide(10, 0), "Error: Division by zero is not allowed.")
+    def test_division(self):
+        self.assertEqual(self.calc.division(10, 2), 5)
+        self.assertEqual(self.calc.division(-10, 2), -5)
+        self.assertEqual(self.calc.division(0, 1), 0)
+        self.assertEqual(self.calc.division(10, 0), "Error: Division by zero is not allowed.")
         
 if __name__ == '__main__':
     unittest.main()
