e159af857fcab42361308ab6b2a19c6d30234669
Muhammed Abdulmalik <abd.muhd01@gmail.com>
2024-07-12 21:52:14 +0300

    Division calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,7 +3,7 @@ def safe_divide(numerator, denominator):
         num = float(numerator)
         denom = float(denominator)
         if denom == 0:
-            return "Error: Division by zero is not allowed."
+            return "Error: Cannot divide by zero."
         result = num / denom
         return f"The result is: {result}"
     except ValueError:
